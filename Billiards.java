a54e32edc7519158752504ce14ee683dff4565d7
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2021-03-10 19:23:15 -0500

    Update Billiards.java

--- Billiards.java
+++ Billiards.java
@@ -3,7 +3,7 @@
  *  Execution:    java Billiards
  *  Dependencies: StdDraw.java StdAudio.java
  *                laser.wav pop.wav
- *                4Ball.png 6Ball.png 8Ball.png
+ *                BilliardsTable.png 4Ball.png 6Ball.png 8Ball.png
  *
  *  GOAL: Upgrade this simulation to run 3 balls at once 
  *  NOTE: It's okay for now to assume that the images do not interact with one another!
@@ -12,35 +12,56 @@
 
 public class Billiards { 
 
-    // TO DO: declare global variables as arrays
+    // declare global array variables
     public static double[] rx;          // x position (m)
     public static double[] ry;          // y position (m)
     public static double[] vx;          // x velocity (m/s)
     public static double[] vy;          // y velocity (m/s)
     public static String[] image;       // name of each ball, including file type
-    
-    // global variables
-    // public static double rx = .480, ry = .880;
-    // public static double vx = .015, vy = .023;
-    // public static String image = "sixBall.png";
-
     public static int N = 3;
 
     public static void main(String[] args) {    
-        // TO DO:   Declare global array size. 
-        // NOTE:    Replace N with the appropriate number.
-
+        // declare size of global arrays
         vx = new double[N];
         vy = new double[N];
         rx = new double[N];
         ry = new double[N];
         image = new String[N];
 
-        // TO DO: initialize global array parameters with a single Loop
-        // NOTE:    Set rx components to -.4, 0, .4
-        //          Set ry components to -1, 0, 1
-        //          Set all vx, vy components to .015, .023 
+        // initialize global arrays
+        init();
+        
+        // initialize StdDraw elements
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setCanvasSize(300, 600);
+        StdDraw.setXscale(-1.0, 1.0);
+        StdDraw.setYscale(-2.0, 2.0);
 
+        // main animation loop
+        while (true) { 
+            step();
+        } 
+    } 
+
+    public static void step() {
+        for (int i = 0; i < N; i++) {
+            // TO DO: do not break until all 3 balls have been pocketed
+            if (pocket(i)) { continue; }
+
+            // TO DO: Upgrade simulation to run all balls that have not been pocketed
+            if (Math.abs(rx[i] + vx[i]) > .75) { vx[i] = -vx[i]; StdAudio.play("pop.wav"); }
+            if (Math.abs(ry[i] + vy[i]) > 1.7) { vy[i] = -vy[i]; StdAudio.play("pop.wav"); }
+
+            rx[i] = rx[i] + vx[i]; 
+            ry[i] = ry[i] + vy[i]; 
+        }
+        draw();
+    }
+
+    // set rx components to -.4, 0, .4
+    // set ry components to -1, 0, 1
+    // set all vx, vy components to .015, .023 
+    public static void init() {
         double rx0 = 0.4, ry0 = 1;
         double vx0 = 0.015, vy0 = 0.023;
         int image0 = 4;
@@ -59,36 +80,11 @@ public class Billiards {
             
             image[i] = (image0 + 2 * i) + "Ball.png";
         }
-
-        StdDraw.enableDoubleBuffering();
-        // set Canvas parameters
-        StdDraw.setCanvasSize(300, 600);
-        StdDraw.setXscale(-1.0, 1.0);
-        StdDraw.setYscale(-2.0, 2.0);
-
-        // main animation loop
-        while (true) { 
-            step();
-        } 
-    } 
-
-    public static void step() {
-        StdDraw.picture(0, 0, "billiardsTable.png", 2, 4);
-        for (int i = 0; i < N; i++) {
-            // TO DO: do not break until all 3 balls have been pocketed
-            if (pocket(i)) { continue; }
-
-            // TO DO: Upgrade simulation to run all balls that have not been pocketed
-            if (Math.abs(rx[i] + vx[i]) > .75) { vx[i] = -vx[i]; StdAudio.play("pop.wav"); }
-            if (Math.abs(ry[i] + vy[i]) > 1.7) { vy[i] = -vy[i]; StdAudio.play("pop.wav"); }
-
-            rx[i] = rx[i] + vx[i]; 
-            ry[i] = ry[i] + vy[i]; 
-        }
-        draw();
     }
 
+    // draw the billiards table with all remaining balls
     public static void draw() {
+        StdDraw.picture(0, 0, "billiardsTable.png", 2, 4);
         for (int i = 0; i < N; i++) {
             StdDraw.picture(rx[i], ry[i], image[i]);
         }
@@ -117,10 +113,14 @@ public class Billiards {
         ry[k] = ry[k] + vy[k];
         StdAudio.play("laser.wav");
 
+        // drop this ball from further iterations
         shift(k);
+
         return;
     }
 
+    // reduce global variable N by 1
+    // shift all elements after k left by 1
     public static void shift(int k) {
         N--;
         for (int i = k; i < N; i++) {
